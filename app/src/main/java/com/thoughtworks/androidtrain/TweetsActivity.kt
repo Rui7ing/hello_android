47c7ec8604f201434578d8204fc70475b217915f
bomu <ruiqing.piao@thoughtworks.com>
2024-04-03 10:21:55 +0800

    feat init data

--- app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/TweetsActivity.kt
@@ -2,8 +2,14 @@ package com.thoughtworks.androidtrain
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.thoughtworks.androidtrain.model.Tweet
+import com.thoughtworks.androidtrain.util.JsonUtil
 
 class TweetsActivity : AppCompatActivity() {
+
+    val recyclerView: RecyclerView by lazy { findViewById(R.id.recycleView) }
+    val tweets: List<Tweet> by lazy { JsonUtil().getTweetList() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
