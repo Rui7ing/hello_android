8b670e6fc128d0cbccdce61272bfb2db81d203a8
bomu <ruiqing.piao@thoughtworks.com>
2024-04-06 15:04:39 +0800

    feat change visibility when click button

--- app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/SharedPreferenceActivity.kt
@@ -2,11 +2,21 @@ package com.thoughtworks.androidtrain
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class SharedPreferenceActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shared_preference_layout)
+        val button = findViewById<Button>(R.id.hintButton)
+        button.setOnClickListener {
+            button.visibility = View.GONE
+            findViewById<TextView>(R.id.hintText).visibility = View.GONE
+            findViewById<TextView>(R.id.spTest).visibility = View.VISIBLE
+        }
+
     }
 }
