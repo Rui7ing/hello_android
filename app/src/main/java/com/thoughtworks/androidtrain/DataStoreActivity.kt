13f12bd159d71b43b53e0c612cf19d55354cfaf9
bomu <ruiqing.piao@thoughtworks.com>
2024-04-06 15:29:30 +0800

    feat change visibility when click button

--- app/src/main/java/com/thoughtworks/androidtrain/DataStoreActivity.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/DataStoreActivity.kt
@@ -2,11 +2,21 @@ package com.thoughtworks.androidtrain
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class DataStoreActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.data_store_layout)
+        val button = findViewById<Button>(R.id.knowButton)
+        button.setOnClickListener {
+            button.visibility = View.GONE
+            findViewById<TextView>(R.id.hintText).visibility = View.GONE
+            findViewById<TextView>(R.id.spTest).visibility = View.VISIBLE
+        }
+
     }
 }
