ded5ad87b077c4d3d3620ebce0d15a2186241480
bomu <ruiqing.piao@thoughtworks.com>
2024-04-10 15:04:59 +0800

    feat by using MutableLiveData

--- app/src/main/java/com/thoughtworks/androidtrain/viewModel/TweetViewModel.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/viewModel/TweetViewModel.kt
@@ -3,11 +3,13 @@ package com.thoughtworks.androidtrain.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.thoughtworks.androidtrain.model.databases.ApplicationDatabase
 import com.thoughtworks.androidtrain.model.entity.Tweet
 import com.thoughtworks.androidtrain.model.repositories.TweetRepository
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 
 class TweetViewModel(application: Application) : AndroidViewModel(application) {
@@ -15,13 +17,13 @@ class TweetViewModel(application: Application) : AndroidViewModel(application) {
     private val tweetRepository: TweetRepository by lazy {
         TweetRepository(ApplicationDatabase(application).tweetDao()) }
 
-    val tweets: LiveData<List<Tweet>> by lazy {
-        tweetRepository.fetchTweets().asLiveData()
-    }
+    private val _tweets = MutableLiveData<List<Tweet>>()
+    val tweets : LiveData<List<Tweet>> get() = _tweets
 
     fun pullData() {
         viewModelScope.launch {
             tweetRepository.saveFromRemote()
+            _tweets.postValue(tweetRepository.fetchTweets().first())
         }
     }
 }
