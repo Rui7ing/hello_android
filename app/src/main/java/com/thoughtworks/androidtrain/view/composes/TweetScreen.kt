6bd38496254a5d651d1d9e3e0c928760461e18bc
bomu <ruiqing.piao@thoughtworks.com>
2024-04-14 12:59:40 +0800

    add dialog item

--- app/src/main/java/com/thoughtworks/androidtrain/view/composes/TweetScreen.kt
+++ app/src/main/java/com/thoughtworks/androidtrain/view/composes/TweetScreen.kt
@@ -2,6 +2,7 @@ package com.thoughtworks.androidtrain.view.composes
 
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,10 +11,14 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.thoughtworks.androidtrain.model.entity.Tweet
 
@@ -31,10 +36,15 @@ fun LoadTweets(tweets: List<Tweet>) {
 
 @Composable
 fun TweetItem(tweet: Tweet) {
+    val openDialog = remember { mutableStateOf(false) }
+    DiaLogItem(openDialog, tweet)
     Row {
         AsyncImage(
             modifier = Modifier
                 .padding(all = 5.dp)
+                .clickable {
+                    openDialog.value = true
+                }
                 .size(60.dp),
             model = tweet.sender?.avatar,
             contentDescription = null
@@ -42,15 +52,37 @@ fun TweetItem(tweet: Tweet) {
         Column {
             Text(
                 modifier = Modifier.padding(vertical = 5.dp),
-                text = tweet.sender?.nick ?: "error name",
+                text = tweet.sender?.nick ?: "",
                 color = Color.Black
             )
             Text(
                 modifier = Modifier.padding(vertical = 2.dp),
-                text = tweet.content ?: "error content",
+                text = tweet.content ?: "",
                 color = Color.Black
             )
         }
+    }
+}
+
+@Composable
+private fun DiaLogItem(
+    openDialog: MutableState<Boolean>,
+    tweet: Tweet
+) {
+    if (!openDialog.value) {
+        return
+    }
+    Dialog(onDismissRequest = { openDialog.value = false }) {
+        AsyncImage(
+            modifier = Modifier
+                .padding(all = 5.dp)
+                .clickable {
+                    openDialog.value = true
+                }
+                .size(300.dp),
+            model = tweet.sender?.avatar,
+            contentDescription = null
+        )
     }
 }
 
